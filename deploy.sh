675e060271e71d52c35f704517c4a6bb3653e6e0
Justin Palma <jpalma@hey.com>
2021-01-31 14:01:47 -0800

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-apk update  && apk add --no-cache curl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl && mv ./kubectl /usr/local/bin/kubectl
-mkdir -p $HOME/.kube
-echo -n $KUBE_CONFIG | base64 -d > $HOME/.kube/config
-kubectl create secret docker-registry gitlab-registry --docker-server="$CI_REGISTRY" --docker-username="$CI_DEPLOY_USER" --docker-password="$CI_DEPLOY_PASSWORD" --docker-email="$GITLAB_USER_EMAIL" -o yaml --dry-run=client
+docker build -t shogunstudios/multi-client:latest -t shogunstudios/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t shogunstudios/multi-server:latest -t shogunstudios/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t shogunstudios/multi-worker:latest -t shogunstudios/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push shogunstudios/multi-client:latest
+docker push shogunstudios/multi-server:latest
+docker push shogunstudios/multi-worker:latest
+
+docker push shogunstudios/multi-client:$SHA
+docker push shogunstudios/multi-server:$SHA
+docker push shogunstudios/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=$CI_REGISTRY_IMAGE/multi-server:$CI_COMMIT_SHORT_SHA
-kubectl set image deployments/client-deployment client=$CI_REGISTRY_IMAGE/multi-client:$CI_COMMIT_SHORT_SHA
-kubectl set image deployments/worker-deployment worker=$CI_REGISTRY_IMAGE/multi-worker:$CI_COMMIT_SHORT_SHA
+kubectl set image deployments/server-deployment server=shogunstudios/multi-server:$SHA
+kubectl set image deployments/client-deployment client=shogunstudios/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=shogunstudios/multi-worker:$SHA
